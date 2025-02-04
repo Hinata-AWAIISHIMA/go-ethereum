3b8511d32bc35687fd96b941e11d50444bc212dd
Jakub Pajek <j.pajek@freebit.net>
2025-02-04 12:10:59 +0900

    Updated simple deployment script

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,13 @@ fi
 
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sudo systemctl stop geth && \
+echo "RUNNING systemctl stop geth" && \
+systemctl stop geth && \
+echo "RUNNING ${SRC_DIR}/uninstall.sh" && \
 ${SRC_DIR}/uninstall.sh && \
+echo "RUNNING ${SRC_DIR}/install.sh" && \
 ${SRC_DIR}/install.sh && \
+echo "RUNNING systemctl start geth" && \
 systemctl start geth && \
+echo "RUNNING journalctl -u geth -f" && \
 journalctl -u geth -f
